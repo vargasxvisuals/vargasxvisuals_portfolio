193301a3afa2fb09f2306fb667c2ea515578e17e
David Vargas <davidvargasproductions@gmail.com>
2026-05-20 23:37:01 -0700

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        display: ['Georgia', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+}
